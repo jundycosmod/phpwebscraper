e0a97961966d8afe3e891c406f978170e905eadf
Jundy Cosmod <jundycosmod@gmail.com>
2020-09-04 21:48:09 +0800

    Added composer.json

--- command.php
+++ command.php
@@ -3,8 +3,27 @@
 require "v2/WebScraper.php";
 
 error_reporting(E_ALL & ~E_NOTICE);
-$webScraper = new WebScraper("https://www.etsy.com/", "q=art%20prints");
+
+//$rootElement = "//ul[contains(@class),'responsive-listing-grid wt-grid wt-grid--block justify-content-flex-start pl-xs-0')]";
+$rootElement = "";
+$webScraper = new WebScraper([
+    'baseUrl' => 'https://www.etsy.com/search',
+    'queries' => 'q=art%20prints',
+    'pagination' => '&ref=pagination&page=',
+    'pages' => 1,
+    'searches' => [
+        'productName' => $rootElement."//div/h3[contains(@class,'text-gray')]",
+        'storeName' => $rootElement."//div/h3[contains(@class,'text-gray-lighter')]",
+        'ratings' => $rootElement."//input[@name='rating']/@value",
+        'numberOfRating' => $rootElement."//div/span/span[contains(@class, 'text-body-smaller text-gray-lighter display-inline-block icon-b-1')]",
+        'price' => $rootElement."//span[contains(concat(' ',normalize-space(@class),' '),'n-listing-card__price')]"
+    ],
+    'export' => 'array'
+]);
+
+// $webScraper = new WebScraper("https://www.etsy.com/", "q=art%20prints");
 // number of pages to be scraped
-$pages = 1;
-$webScraper->saveItems("search", $pages);
-$webScraper->saveStores("shop/");
+// $pages = 1;
+$result = $webScraper->get();
+var_dump($result);
+// $webScraper->saveStores("shop/");
--- v2/WebScraper.php
+++ v2/WebScraper.php
@@ -2,139 +2,65 @@
 
 class WebScraper {
 
-    protected $baseUrl;
+    protected $options;
 
-    protected $query;
-
-    protected $stores;
-
-    public function __construct($baseUrl, $query)
+    public function __construct($options)
     {
-        $this->baseUrl = $baseUrl;
-        $this->query = $query;
+        $this->options = $options;
     }
 
-    public function getItems($path, $pages)
+    public function get()
     {
         $items = [];
         $this->stores = [];
-        for($i = 1; $i <= $pages; $i++){
-            $pageQuery = $i ? "&ref=pagination&page=".$i : "";
-            $html = $this->getHtmlContents($this->baseUrl.$path, $this->query.$pageQuery, "GET");
+        for($i = 1; $i <= $this->options['pages']; $i++){
+            $pageQuery = $i ? $this->options['pagination'].$i : "";
+            $html = $this->getHtmlContents($this->options['baseUrl'], $this->options['queries'].$pageQuery, "GET");
 
             if($xpath = $this->getXPath($html)){
-
-                $rootElement = "//ul[contains(concat(' ',normalize-space(@class),' '),'responsive-listing-grid wt-grid wt-grid--block justify-content-flex-start pl-xs-0')]";
-    
-                //get all the product names
-                $productNames = $xpath->query($rootElement."//div/h3[contains(concat(' ',normalize-space(@class),' '),'text-gray')]");
-                $storeNames = $xpath->query($rootElement."//p[contains(concat(' ',normalize-space(@class),' '),'text-gray-lighter')]");
-                $ratings = $xpath->query($rootElement."//input[@name='rating']/@value");
-                $numberOfRatings = $xpath->query($rootElement."//div/span/span[contains(concat(' ',normalize-space(@class),' '),'text-body-smaller text-gray-lighter display-inline-block icon-b-1')]");
-                $prices = $xpath->query($rootElement."//span[contains(concat(' ',normalize-space(@class),' '),'n-listing-card__price')]");
-
-
-                $counter = 0;
-                $salesPricePattern = "Sale Price";
-                if($productNames->length > 0){
-                    foreach($productNames as $row){
-
-                        $price = explode("\n", $prices[$counter]->nodeValue);
-                        if(!empty($price))
-                        {
-                            $price = preg_grep('/'.$salesPricePattern.'\s.*/', $price);
-
-                            if(!empty($price)){
-                                foreach($price as $salePrice){
-                                    
-                                    $price = str_replace($salesPricePattern, "", $salePrice);
-                                }
-                                
-                            }else{
-                                
-                                $price = $prices[$counter]->nodeValue;
-                            }
-                            
-                        }
-                        
-
-                        $items[] = [
-                            'productName' => trim($row->nodeValue),
-                            'storeName' => trim($storeNames[$counter]->nodeValue),
-                            'ratings' => trim($ratings[$counter]->nodeValue),
-                            'numberOfRating' => trim($numberOfRatings[$counter]->nodeValue),
-                            'price' => trim(str_replace("FREE shipping", "", $price))
-                        ];
-                        $this->stores[trim($storeNames[$counter]->nodeValue)] = trim($storeNames[$counter]->nodeValue);
-                        $counter++;
-                    }
-                }
+                
+                $items = $this->parseData($xpath);
+               
             }   
         }
         
         return $items;
     }
 
-    public function getStores($path){
-
-        $storeDetails = [];
-        foreach($this->stores as $store){
-            $html = $this->getHtmlContents($this->baseUrl.$path. $store, "", "GET");
- 
-            if($xpath = $this->getXPath($html)){
-                $storeHeadline = $xpath->query("//span[contains(concat(' ',normalize-space(@data-key),' '),'headline')]");
-                $storeLocation = $xpath->query("//span[contains(concat(' ',normalize-space(@data-key),' '),'user_location')]");
-                $storeSales = $xpath->query("//div/span[contains(concat(' ',normalize-space(@class),' '),'wt-text-body-01 no-wrap')]");
-                $storeRatings = $xpath->query("//input[@name='rating']/@value");
-                $storeFaves = $xpath->query("//a[contains(@href,'/favoriters')]");
-
-                if($storeHeadline->length > 0){
-                    foreach($storeHeadline as $row){
-
-                        $storeDetails[$store] = [
-                            'storeName' => $store,
-                            'storeHeadline' => trim($row->nodeValue),
-                            'storeLocation' => trim($storeLocation[0]->nodeValue),
-                            'storeSales' => intval(trim($storeSales[0]->nodeValue)),
-                            'storeRatings' => trim($storeRatings[0]->nodeValue),
-                            'storeFaves' => intval(trim($storeFaves[0]->nodeValue))
-                        ];
+    protected function parseData($xpath)
+    {
+        $results = [];
+        foreach($this->options['searches'] as $searchKey => $searchFilter){
+            $searchResults = $xpath->query($searchFilter);
             
-                    }
+            if($searchResults->length > 0){
+                $counter = 0;
+                foreach($searchResults as $searchResult){
+                    $results[$counter][$searchKey] = trim($searchResult->nodeValue);
+                    $counter++;
                 }
+                
             }
             
         }
-        return $storeDetails;
-    }
-
-    public function saveStores($path){
-        $csv = fopen('stores.csv', 'w');
-        $stores = $this->getStores($path);
-
-        if(!empty($stores)){
-            foreach ($stores as $fields) {
-                fputcsv($csv, $fields);
-            }
-        }
         
-        fclose($csv);
-    }
-
-    public function saveItems($path, $numberOfPages){
-        $csv = fopen('items.csv', 'w');
-        $items = $this->getItems($path, $numberOfPages);
-
-        if(!empty($items)){
-            foreach ($items as $fields) {
-                fputcsv($csv, $fields);
-            }
+        if($this->options['export'])
+        {
+            $exportOption = "to".ucfirst($this->options['export']);
+            return $this->$exportOption($results);
         }
-        
-        fclose($csv);
+
+        return $results;
+
     }
 
-    protected function getXPath($html){
+    protected function toArray($output)
+    {
+        return $output;
+    }
+
+    protected function getXPath($html)
+    {
         $doc = new DOMDocument();
 
         libxml_use_internal_errors(TRUE); 
@@ -150,11 +76,8 @@ class WebScraper {
         return null;
     }
 
-    protected function getHtmlContents($baseUrl, $query, $method) {
-
-        if (strpos($baseUrl, 'shop') !== false) {
-            return file_get_contents($baseUrl.$query);
-        }
+    protected function getHtmlContents($baseUrl, $query, $method) 
+    {
 
         $url = $baseUrl;
         if ($query != "") {
@@ -165,6 +88,7 @@ class WebScraper {
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
         $output = curl_exec($ch);
         curl_close($ch);
 
